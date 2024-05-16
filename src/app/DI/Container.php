1f7926fac9ddb842b3f23e86b88dbabe2ef9eb89
Artur <fartur09@mail.ru>
2024-05-16 21:21:56 +0500

    Added not worked DI conteiner. Added auth middleware.

--- src/app/DI/Container.php
+++ src/app/DI/Container.php
@@ -2,25 +2,102 @@
 
 namespace Dangje\WebFramework\DI;
 
-class Container
+use Psr\Container\ContainerInterface;
+use ReflectionClass;
+
+class Container implements ContainerInterface
 {
-    private array $services = [];
+    private $objects = [];
 
-    public function set(string $id, $service): void
-    {
-        $this->services[$id] = $service;
+    public function has(string $id): bool {
+        return isset($this->objects[$id]) || class_exists($id);
     }
 
-    public function get(string $id): object
+    public function get(string $id)
     {
-        if (!isset($this->services[$id])) {
-            throw new \InvalidArgumentException('No service registered for id ' . $id);
+        return 
+            isset($this->objects[$id]) 
+            ? $this->objects[$id]()
+            : $this->prepareObject($id);
+    }
+
+    // private function set(string $class, ...$args): object {
+    //     $object = $args ? new $class(...$args) : new $class();
+    //     $this->objects[$class] = $object;
+    //     return $object;
+    // }
+
+    private function prepareObject(string $class): object {
+        $classReflector = new ReflectionClass($class);
+
+        $construct = $classReflector->getConstructor();
+        if (empty($construct)) {
+            // return $this->set($class);
+            return new $class();
         }
-        return $this->services[$id];
-    }
 
-    public function has($id): bool
-    {
-        return array_key_exists($id, $this->services);
+        $constructArguments = $construct->getParameters();
+        if (empty($constructArguments)) {
+            // return $this->set($class);
+            return new $class();
+        }
+
+        $args = [];
+        foreach ($constructArguments as $parameter) {
+            $parameterType = $parameter->getType();
+
+            if ($parameterType !== null) {
+                $parameterTypeName = $parameterType->getName();
+
+                if (class_exists($parameterTypeName)) {
+                    $args[$parameter->getName()] = $this->get($parameterTypeName);
+                    if ($parameterTypeName === 'int') {
+                        $args[$parameter->getName()] = 0;
+                    } elseif ($parameterTypeName === 'string') {
+                        $args[$parameter->getName()] = '';
+                    } elseif ($parameterTypeName === 'bool') {
+                        $args[$parameter->getName()] = false;
+                    } elseif ($parameterTypeName === 'float') {
+                        $args[$parameter->getName()] = 0.0;
+                    } else {
+                        throw new \Exception("Неизвестный тип параметра '$parameterTypeName' для параметра '{$parameter->getName()}'.");
+                    }
+                }
+            } else {
+                if ($parameter->isDefaultValueAvailable()) {
+                    $args[$parameter->getName()] = $parameter->getDefaultValue();
+                } else {
+                    throw new \Exception("Параметр '{$parameter->getName()}' не имеет типа и значения по умолчанию.");
+                }
+            }
+        }
+        
+        // foreach ($constructArguments as $argument) {
+
+        //     $argumentType = $argument->getType();
+
+        //     $argumentName = $argument->getType()->getName();
+
+        //     if (class_exists($argumentType)) {
+        //         $args[$argumentName] = $this->get($argumentType);
+        //     } else {
+        //         if ($argumentType === 'int') {
+        //             $args[$argumentName] = 0;
+        //         } elseif ($argumentType === 'string') {
+        //             $args[$argumentName] = '';
+        //         } elseif ($argumentType === 'bool') {
+        //             $args[$argumentName] = false;
+        //         } elseif ($argumentType === 'float') {
+        //             $args[$argumentName] = 0.0;
+        //         } else {
+        //             throw new \Exception("Неизвестный тип параметра '$argumentType' для параметра '{$argumentName}'.");
+        //         }
+        //     }
+
+        //     $args[$argument->getName()] = $this->get($argumentType);
+        // }
+
+        //return $this->set($class, ...$args);
+        return new $class(...$args);
     }
 }
--- web/index.php
+++ web/index.php
@@ -1,43 +1,55 @@
 <?php
 
 use Dangje\WebFramework\Message\Stream;
+use Dangje\WebFramework\App;
+use Dangje\WebFramework\DI\Container;
+use Dangje\WebFramework\Handler\Request;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
+
+$conteiner = new Container();
+
+$request = $conteiner->get(Request::class);
+
+echo $request->getMethod();
 
 /*$app = new App();
 echo $app->run();
 */
+
+//$entityBody = file_get_contents('php://input');
 
 
 $st = new Stream('php://input');
 
 echo $st->getSize();
 
+// echo $entityBody;
 
-foreach ($_SERVER as $key => $value) {
-    if(is_array($value)){
-        echo '<pre>';
-        echo "{$key} => ... ";
-        echo '</pre>';
-        foreach ($value as $key1 => $value1) {
-            echo '<pre>';
-            echo "{$key1} => {$value1} ";
-            echo '</pre>';
-        }
-    }
-    echo '<pre>';
-    echo "{$key} => {$value} ";
-    echo '</pre>';
-}
+// foreach ($_SERVER as $key => $value) {
+//     if(is_array($value)){
+//         echo '<pre>';
+//         echo "{$key} => ... ";
+//         echo '</pre>';
+//         foreach ($value as $key1 => $value1) {
+//             echo '<pre>';
+//             echo "{$key1} => {$value1} ";
+//             echo '</pre>';
+//         }
+//     }
+//     echo '<pre>';
+//     echo "{$key} => {$value} ";
+//     echo '</pre>';
+// }
 
 
-foreach ($_GET as $key => $value) {
-    echo '<pre>';
-    echo "{$key} => {$value} ";
-    echo '</pre>';
-}
+// foreach ($_GET as $key => $value) {
+//     echo '<pre>';
+//     echo "{$key} => {$value} ";
+//     echo '</pre>';
+// }
 
-function some ()
-{
-    return "ret";
-}
+// function some ()
+// {
+//     return "ret";
+// }
