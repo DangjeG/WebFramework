91222c74a1277b15cd8e5787d8e610d23fda1591
Artur <fartur09@mail.ru>
2024-05-16 21:21:56 +0500

    Added not worked DI conteiner. Added auth middleware.

--- src/app/DI/Container.php
+++ src/app/DI/Container.php
@@ -2,25 +2,46 @@
 
 namespace Dangje\WebFramework\DI;
 
-class Container
-{
-    private array $services = [];
+use Psr\Container\ContainerInterface;
 
-    public function set($service): void
+/**
+ * Example config file
+ * return [
+ *      'Car' => function (\Psr\Container\ContainerInterface $container) {
+ *          return new Dangje\WebFramework\DI\Car($container->get('Engine'));
+ *      },
+ *      'Engine' => function () {
+ *          return new \Dangje\WebFramework\DI\Engine();
+ *      }
+ *  ];
+ */
+
+class Container implements ContainerInterface
+{
+    private $definitions = [];
+    private $instances = [];
+
+    public function __construct(array $definitions)
     {
-        $this->services[$service::class] = $service;
+        $this->definitions = $definitions;
     }
 
-    public function get($id): object
+    public function get($id)
     {
-        if (!isset($this->services[$id])) {
-            throw new \InvalidArgumentException('No service registered for id ' . $id);
+        if (isset($this->instances[$id])) {
+            return $this->instances[$id];
         }
-        return $this->services[$id];
+
+        if (isset($this->definitions[$id])) {
+            $this->instances[$id] = $this->definitions[$id]($this);
+            return $this->instances[$id];
+        }
+
+        throw new \Exception("Service not found: " . $id);
     }
 
     public function has($id): bool
     {
-        return array_key_exists($id, $this->services);
+        return isset($this->definitions[$id]);
     }
 }
